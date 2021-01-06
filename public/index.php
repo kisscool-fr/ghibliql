e66d731153c1d866407053524bb5e92748644bef
KisSCoOl <kisscool@kisscool.net>
2021-01-06 14:27:33 +0100

    (issue) fix Debug name

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ use \GhibliQL\Data\DataSource;
 use \GraphQL\Type\Schema;
 use \GraphQL\GraphQL;
 use \GraphQL\Error\FormattedError;
-use \GraphQL\Error\Debug;
+use \GraphQL\Error\DebugFlag;
 
 // Disable default PHP error reporting - we have better one for debug mode (see bellow)
 ini_set('display_errors', 0);
@@ -21,7 +21,7 @@ if (!empty($_GET['debug'])) {
     set_error_handler(function($severity, $message, $file, $line) use (&$phpErrors) {
         throw new ErrorException($message, 0, $severity, $file, $line);
     });
-    $debug = Debug::INCLUDE_DEBUG_MESSAGE | Debug::INCLUDE_TRACE;
+    $debug = DebugFlag::INCLUDE_DEBUG_MESSAGE | DebugFlag::INCLUDE_TRACE;
 }
 
 try {
